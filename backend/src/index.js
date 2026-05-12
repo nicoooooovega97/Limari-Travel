4dfbfe5a24a35d3a7099429fbca4be1d26f79458
nicoooooovega97 <91704410+nicoooooovega97@users.noreply.github.com>
2026-05-12 19:46:58 -0400

    Revert "arreglo backend"
    This reverts commit 89746049f3506b8887daf62443fcf5b1cd3fe55d.

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,14 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ 
-  origin: [
-    "http://localhost:5173", 
-    "https://limari-travel.vercel.app",
-    "https://limari-travel.vercel.app"  
-  ], 
-  credentials: true 
-}));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
