89746049f3506b8887daf62443fcf5b1cd3fe55d
nicoooooovega97 <91704410+nicoooooovega97@users.noreply.github.com>
2026-05-12 13:35:39 -0400

    arreglo backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ 
+  origin: [
+    "http://localhost:5173", 
+    "https://limari-travel.vercel.app",
+    "https://limari-travel.vercel.app"  
+  ], 
+  credentials: true 
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
